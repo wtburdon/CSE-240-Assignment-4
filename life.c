8d9ab927c494b64f16c5f821f3024515d1c2c506
wtburdon <billyburdon@outlook.com>
2025-02-07 11:06:44 -0700

    created PrintBoard function

--- life.c
+++ life.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#define NUM_ROWS 5
+#define NUM_COLS 5
 
+typedef enum {F = 0, T = 1} Bool;
+
+
+void PrintBoard(char board[][NUM_COLS])
+{
+
+}
+
+void PlayGameOfLife()
+{
+
+}
 
 int main(){
     

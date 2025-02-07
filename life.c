77fb06639d4400de0865296ce2d4d7792c914c23
wtburdon <billyburdon@outlook.com>
2025-02-07 11:09:46 -0700

    created SetInitialState function

--- life.c
+++ life.c
@@ -4,9 +4,14 @@
 
 typedef enum {F = 0, T = 1} Bool;
 
-int CountLiveNeighbors(char board[][NUM_COLS], int row, int col)
+void SetInitialState(char board[][NUM_COLS], int init_state[], int num_alive)
 {
     
+}
+
+int CountLiveNeighbors(char board[][NUM_COLS], int row, int col)
+{
+
 }
 
 void NextGeneration(char board[][NUM_COLS])
